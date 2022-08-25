f929a0dbea033f8a95b4d9f2e48632218413f970
blaketingyu <blaketingyu@gmail.com>
2022-08-25 22:24:29 +0800

    chore: add shortcut for app.listen()

--- app/index.js
+++ app/index.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const path = require('path');
-const pug = require('pug');
+const express = require("express");
+const path = require("path");
+const pug = require("pug");
 
 const app = express();
-app.use(express.static('public'));
-app.engine('html', pug.renderFile);
-app.set('view engine', 'pug');
+app.use(express.static("public"));
+app.engine("html", pug.renderFile);
+app.set("view engine", "pug");
 
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
-app.get('/', (req, res) => {
-  res.render(path.join(__dirname + '/index.pug'));
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname + "/index.pug"));
 });
 
-app.get('/success', (req, res) => {
-  res.send('Hello World Success!');
-})
+app.get("/success", (req, res) => {
+  res.send("Hello World Success!");
+});
 
 app.listen(PORT, HOST, () => {
   console.log(`Example app listening on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
+  console.log(`Testing NGINX Reverse Proxy: http://localhost:80`);
 });
